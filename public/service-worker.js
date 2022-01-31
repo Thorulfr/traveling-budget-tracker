cb124f0dac37e667087f736cdf3a6081d258b655
Ben H <deepfreespace@gmail.com>
2022-01-30 20:26:41 -0700

    Service worker complete

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,11 +19,50 @@ const FILES_TO_CACHE = [
     './icons/icon-512x512.png',
 ];
 
+// Install and store files to cache
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME);
+            console.log('Installing cache : ' + CACHE_NAME);
             return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// Handle old versions of cache
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('Deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Retrieve files from cache
+self.addEventListener('fetch', function (e) {
+    console.log('Fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                // If cached files are available, respond with cache
+                console.log('Responding with cache : ' + e.request.url);
+                return request;
+            } else {
+                // If there are no cached files, try fetching
+                console.log('File is not cached, fetching : ' + e.request.url);
+                return fetch(e.request);
+            }
         })
     );
 });
